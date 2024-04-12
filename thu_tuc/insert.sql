69b4af6b9b526c852ceeb1a64e864d3a74c25827
QuanCters <quan.nguyen2212804@hcmut.edu.vn>
2024-04-12 18:40:44 +0700

    add create table

--- thu_tuc/insert.sql
+++ thu_tuc/insert.sql
@@ -1,3 +1,4 @@
 USE assignment2;
 SET DATEFORMAT dmy;
+insert into benh_nhan values (0000000001,079204029573,'Nguyen','Quan','111/83','quan@gmail.com','Nam','25-07-2004')
 insert into benh_nhan values (0000000002,0123456789502,'Nguyen','Nhu','DBP','NHU@gmail.com','Nu','22-02-2004')
