36cb990c8e3a71a7fc6697fde7d26a193a245107
cbungvu963 <167999369+cbungvu963@users.noreply.github.com>
2024-05-09 09:06:46 +0700

    Add files via upload
    Chỉnh lại lan_di_benh_vien, dich_vu_luu_tru, thuoc, don_thuoc

---
+++ Update_BN_TongTienThuoc.sql
@@ -0,0 +1,326 @@
+SELECT * FRom don_thuoc
+
+SELECT * FRom dich_vu_luu_tru
+
+
+UPDATE lan_di_benh_vien
+SET Tong_chi_phi = 
+    CASE 
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10001' THEN 4450000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10002' THEN 5200000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10003' THEN 4875000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10004' THEN 6100000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10005' THEN 7850000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10006' THEN 2420000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10007' THEN 1850000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10008' THEN 1215000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10009' THEN 2195000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10010' THEN 4980000
+         WHEN Ma_so_lan_di_benh_vien = 'LDBV10001' THEN 4450000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10002' THEN 5200000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10003' THEN 4875000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10004' THEN 6100000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10005' THEN 7850000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10006' THEN 2420000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10007' THEN 1850000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10008' THEN 1215000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10009' THEN 2195000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10010' THEN 4980000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10011' THEN 9110000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10012' THEN 3655000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10013' THEN 2460000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10014' THEN 2220000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10015' THEN 2675000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10016' THEN 1230000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10017' THEN 3500000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10018' THEN 1565000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10019' THEN 1900000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10020' THEN 2300000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10021' THEN 2450000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10022' THEN 1875000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10023' THEN 975000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10024' THEN 874000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10025' THEN 1990000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10026' THEN 2050000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10027' THEN 1350000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10028' THEN 7850000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10029' THEN 2760000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10030' THEN 2755000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10031' THEN 2225000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10032' THEN 1550000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10033' THEN 975000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10034' THEN 1450000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10035' THEN 1650000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10036' THEN 2000000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10037' THEN 1765000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10038' THEN 2320000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10039' THEN 2055000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10040' THEN 2400000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10041' THEN 975000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10042' THEN 2455000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10043' THEN 1875000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10044' THEN 1055000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10045' THEN 1485000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10046' THEN 2950000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10047' THEN 1050000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10048' THEN 975000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10049' THEN 3950000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10050' THEN 4450000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10051' THEN 2650000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10052' THEN 2350000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10053' THEN 1760000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10054' THEN 3450000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10055' THEN 1430000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10056' THEN 1550000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10057' THEN 1650000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10058' THEN 2950000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10059' THEN 1750000
+        WHEN Ma_so_lan_di_benh_vien = 'LDBV10060' THEN 1540000
+    END;
+GO
+
+
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-04-10',
+    Tong_chi_phi = 6665000
+WHERE Ma_so = 'LT12001';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-04-17',
+    Tong_chi_phi = 7310000
+WHERE Ma_so = 'LT12002';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-04-20',
+    Tong_chi_phi = 7095000
+WHERE Ma_so = 'LT12003';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-04-24',
+    Tong_chi_phi = 7095000
+WHERE Ma_so = 'LT12004';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-04-28',
+    Tong_chi_phi = 7310000
+WHERE Ma_so = 'LT12005';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-04-30',
+    Tong_chi_phi = 7095000
+WHERE Ma_so = 'LT12006';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-04-30',
+    Tong_chi_phi = 6450000
+WHERE Ma_so = 'LT14001';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-05-01',
+    Tong_chi_phi = 6235000
+WHERE Ma_so = 'LT14002';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-05-01',
+    Tong_chi_phi = 5375000
+WHERE Ma_so = 'LT14003';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-05-02',
+    Tong_chi_phi = 4945000
+WHERE Ma_so = 'LT14004';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-05-02',
+    Tong_chi_phi = 4300000
+WHERE Ma_so = 'LT16001';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-05-02',
+    Tong_chi_phi = 3010000
+WHERE Ma_so = 'LT16002';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-05-03',
+    Tong_chi_phi = 1935000
+WHERE Ma_so = 'LT16003';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-05-04',
+    Tong_chi_phi = 2150000
+WHERE Ma_so = 'LT16004';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-05-04',
+    Tong_chi_phi = 2150000
+WHERE Ma_so = 'LT16005';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-05-04',
+    Tong_chi_phi = 2150000
+WHERE Ma_so = 'LT16006';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-05-05',
+    Tong_chi_phi = 2150000
+WHERE Ma_so = 'LT16007';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-05-05',
+    Tong_chi_phi = 1935000
+WHERE Ma_so = 'LT16008';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-05-05',
+    Tong_chi_phi = 1935000
+WHERE Ma_so = 'LT16009';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-05-06',
+    Tong_chi_phi = 2150000
+WHERE Ma_so = 'LT16010';
+
+UPDATE dich_vu_luu_tru
+SET 
+    Ngay_ket_thuc = '2024-05-06',
+    Tong_chi_phi = 1935000
+WHERE Ma_so = 'LT16011';
+
+GO
+
+
+UPDATE thuoc
+SET Gia_ban = 
+    CASE 
+        WHEN Ma_thuoc = 'TH00001' THEN 1000
+        WHEN Ma_thuoc = 'TH00002' THEN 1500
+        WHEN Ma_thuoc = 'TH00003' THEN 1000
+        WHEN Ma_thuoc = 'TH00004' THEN 1000
+        WHEN Ma_thuoc = 'TH00005' THEN 1000
+        WHEN Ma_thuoc = 'TH00006' THEN 500
+        WHEN Ma_thuoc = 'TH00007' THEN 900
+        WHEN Ma_thuoc = 'TH00008' THEN 24000
+        WHEN Ma_thuoc = 'TH00009' THEN 500
+        WHEN Ma_thuoc = 'TH00010' THEN 2200
+        WHEN Ma_thuoc = 'TH00011' THEN 600
+        WHEN Ma_thuoc = 'TH00012' THEN 500
+        WHEN Ma_thuoc = 'TH00013' THEN 9000
+        WHEN Ma_thuoc = 'TH00014' THEN 2000
+        WHEN Ma_thuoc = 'TH00015' THEN 1300
+        WHEN Ma_thuoc = 'TH00016' THEN 1700
+        WHEN Ma_thuoc = 'TH00017' THEN 5500
+        WHEN Ma_thuoc = 'TH00018' THEN 5000
+        WHEN Ma_thuoc = 'TH00019' THEN 850
+        WHEN Ma_thuoc = 'TH00020' THEN 1000
+    END;
+GO
+
+
+UPDATE don_thuoc
+SET Tong_tien_thuoc = 
+    CASE 
+        WHEN Ma_don_thuoc = 'DT10001' THEN 10000
+        WHEN Ma_don_thuoc = 'DT10002' THEN 22500
+        WHEN Ma_don_thuoc = 'DT10003' THEN 20000
+        WHEN Ma_don_thuoc = 'DT10004' THEN 10000
+        WHEN Ma_don_thuoc = 'DT10005' THEN 5000
+        WHEN Ma_don_thuoc = 'DT10006' THEN 6000
+        WHEN Ma_don_thuoc = 'DT10007' THEN 7200
+        WHEN Ma_don_thuoc = 'DT10008' THEN 360000
+        WHEN Ma_don_thuoc = 'DT10009' THEN 5000
+        WHEN Ma_don_thuoc = 'DT10010' THEN 44000
+        WHEN Ma_don_thuoc = 'DT10011' THEN 10800
+        WHEN Ma_don_thuoc = 'DT10012' THEN 6000
+        WHEN Ma_don_thuoc = 'DT10013' THEN 63000
+        WHEN Ma_don_thuoc = 'DT10014' THEN 18000
+        WHEN Ma_don_thuoc = 'DT10015' THEN 19500
+        WHEN Ma_don_thuoc = 'DT10016' THEN 34000
+        WHEN Ma_don_thuoc = 'DT10017' THEN 55000
+        WHEN Ma_don_thuoc = 'DT10018' THEN 40000
+        WHEN Ma_don_thuoc = 'DT10019' THEN 10200
+        WHEN Ma_don_thuoc = 'DT10020' THEN 15000
+	    WHEN Ma_don_thuoc = 'DT10021' THEN 10000
+        WHEN Ma_don_thuoc = 'DT10022' THEN 22500
+        WHEN Ma_don_thuoc = 'DT10023' THEN 20000
+        WHEN Ma_don_thuoc = 'DT10024' THEN 10000
+        WHEN Ma_don_thuoc = 'DT10025' THEN 5000
+        WHEN Ma_don_thuoc = 'DT10026' THEN 6000
+        WHEN Ma_don_thuoc = 'DT10027' THEN 7200
+        WHEN Ma_don_thuoc = 'DT10028' THEN 360000
+        WHEN Ma_don_thuoc = 'DT10029' THEN 5000
+        WHEN Ma_don_thuoc = 'DT10030' THEN 44000
+        WHEN Ma_don_thuoc = 'DT10031' THEN 10800
+        WHEN Ma_don_thuoc = 'DT10032' THEN 6000
+        WHEN Ma_don_thuoc = 'DT10033' THEN 63000
+        WHEN Ma_don_thuoc = 'DT10034' THEN 18000
+        WHEN Ma_don_thuoc = 'DT10035' THEN 19500
+        WHEN Ma_don_thuoc = 'DT10036' THEN 34000
+        WHEN Ma_don_thuoc = 'DT10037' THEN 55000
+        WHEN Ma_don_thuoc = 'DT10038' THEN 40000
+        WHEN Ma_don_thuoc = 'DT10039' THEN 10200
+        WHEN Ma_don_thuoc = 'DT10040' THEN 15000
+        WHEN Ma_don_thuoc = 'DT10041' THEN 10000
+        WHEN Ma_don_thuoc = 'DT10042' THEN 22500
+        WHEN Ma_don_thuoc = 'DT10043' THEN 20000
+        WHEN Ma_don_thuoc = 'DT10044' THEN 10000
+        WHEN Ma_don_thuoc = 'DT10045' THEN 5000
+        WHEN Ma_don_thuoc = 'DT10046' THEN 6000
+        WHEN Ma_don_thuoc = 'DT10047' THEN 7200
+        WHEN Ma_don_thuoc = 'DT10048' THEN 360000
+        WHEN Ma_don_thuoc = 'DT10049' THEN 5000
+        WHEN Ma_don_thuoc = 'DT10050' THEN 44000
+        WHEN Ma_don_thuoc = 'DT10051' THEN 10800
+        WHEN Ma_don_thuoc = 'DT10052' THEN 6000
+        WHEN Ma_don_thuoc = 'DT10053' THEN 63000
+        WHEN Ma_don_thuoc = 'DT10054' THEN 18000
+        WHEN Ma_don_thuoc = 'DT10055' THEN 19500
+        WHEN Ma_don_thuoc = 'DT10056' THEN 34000
+        WHEN Ma_don_thuoc = 'DT10057' THEN 55000
+        WHEN Ma_don_thuoc = 'DT10058' THEN 40000
+        WHEN Ma_don_thuoc = 'DT10059' THEN 10200
+        WHEN Ma_don_thuoc = 'DT10060' THEN 15000
+    END;
+GO
+
+
+
+UPDATE don_thuoc
+SET Tong_tien_thuoc = 0
+WHERE Ma_don_thuoc = 'DT01111'   
+
+UPDATE don_thuoc
+SET Tong_tien_thuoc = 0
+WHERE Ma_don_thuoc = 'DT01112'   
+
+UPDATE don_thuoc
+SET Tong_tien_thuoc = 0
+WHERE Ma_don_thuoc = 'DT01113'   
+
+UPDATE don_thuoc
+SET Tong_tien_thuoc = 0
+WHERE Ma_don_thuoc = 'DT01114'   
+
+UPDATE don_thuoc
+SET Tong_tien_thuoc = 0
+WHERE Ma_don_thuoc = 'DT01115'   
