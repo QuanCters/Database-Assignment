fb03ba99a7dc3e7dee9cf9f99632751e1584a656
cbungvu963 <167999369+cbungvu963@users.noreply.github.com>
2024-05-09 08:13:36 +0700

    Update Code_Insert_trigger_SoBenhNhan_HienTai.sql

--- BTL_Trigger/Code_Insert_trigger_SoBenhNhan_HienTai.sql
+++ BTL_Trigger/Code_Insert_trigger_SoBenhNhan_HienTai.sql
@@ -1,205 +1,161 @@
-DROP TRIGGER UpdateSoLuongBenhNhan_phongBenh
-DROP TRIGGER UpdateDeleteSoLuongBenhNhan_phongBenh
+INSERT INTO lan_su_dung_dich_vu (Ma_so, Ma_so_lan_di_benh_vien, Ma_loai_dich_vu, Ma_so_nhan_vien)
+VALUES ('LT12007', 'LDBV10050', 'LT', 'BS10002'),
+	   ('LT12008', 'LDBV10051', 'LT', 'BS10002'),
+	   ('LT12009', 'LDBV10052', 'LT', 'BS10003'),
+	   ('LT12010', 'LDBV10053', 'LT', 'BS10003'),
+	   ('LT12011', 'LDBV10054', 'LT', 'BS10004'),
+	   ('LT12012', 'LDBV10055', 'LT', 'BS10004'),
+	   ('LT14005', 'LDBV10056', 'LT', 'BS10005'),
+	   ('LT14006', 'LDBV10057', 'LT', 'BS10005'),
+	   ('LT14007', 'LDBV10058', 'LT', 'BS10005');
 
-DROP TRIGGER SoLuongBN_HienTai_NgayKetThuc
-GO 
-USE assignment2T
+INSERT INTO dich_vu_luu_tru (Ma_so, Ngay_bat_dau, Ngay_ket_thuc, Tong_chi_phi)
+VALUES 
+('LT12007', '2024-04-19', '2024-04-30', 2150000),
+('LT12008', '2024-04-22', '2024-05-01', 2580000),
+('LT12009', '2024-05-02', '2024-05-20', 3870000),
+('LT12010', '2024-05-03', '2024-05-24', 5160000),
+('LT12011', '2024-05-03', '2024-05-24', 5160000),
+('LT12012', '2024-05-03', '2024-05-24', 5160000),
+('LT14005', '2024-05-03', '2024-05-24', 5160000),
+('LT14006', '2024-05-03', '2024-05-24', 5160000),
+('LT14007', '2024-05-03', '2024-05-24', 5160000);
 
------- TỰ ĐỘNG TĂNG SoLuongBN_HienTai khi insert đúng vào table dbo.phong_benh_duoc_su_dung_tai_dich_vu_luu_tru
+SELECT * FROM dich_vu_luu_tru
+--TH1: Bệnh nhân vẫn còn hạn sử dụng DV lưu trú, phòng bệnh muốn thêm còn chỗ
+SELECT * FROM phong_benh
 
-CREATE TRIGGER UpdateSoLuongBenhNhan_phongBenh
-ON dbo.phong_benh_duoc_su_dung_tai_dich_vu_luu_tru
-FOR INSERT
-AS
-BEGIN
-    DECLARE @SoBenhNhan INT;
-    DECLARE @LoaiPhong INT;
-    DECLARE @NgayKetThuc DATE;
-    
-    SELECT @SoBenhNhan = pb.So_luong_benh_nhan_hien_tai, 
-           @LoaiPhong = pb.Loai_phong,
-           @NgayKetThuc = dvl.Ngay_ket_thuc
-    FROM dbo.phong_benh pb
-    INNER JOIN inserted i ON i.So_phong = pb.So_phong
-    INNER JOIN dbo.dich_vu_luu_tru dvl ON dvl.Ma_so = i.Ma_so_dich_vu_luu_tru;
-
-    IF @NgayKetThuc < GETDATE()
-    BEGIN
-        PRINT N'Không thể thêm bệnh nhân vào phòng bệnh do đã hết hạn DV lưu trú';
-        ROLLBACK TRANSACTION;
-        RETURN;
-    END
-
-    -- Check phòng bệnh đã full ch? 
-    IF @SoBenhNhan >= @LoaiPhong
-    BEGIN
-        RAISERROR(N'Phòng đã đủ số lượng - Vui lòng chọn phòng khác', 16, 1);
-        ROLLBACK TRANSACTION;
-        RETURN;
-    END
-
-    -- Update bệnh nhân nếu thỏa hết đkien ở trên
-    ELSE
-    BEGIN
-        UPDATE dbo.phong_benh
-        SET So_luong_benh_nhan_hien_tai = So_luong_benh_nhan_hien_tai + 1
-        WHERE So_phong = (SELECT So_phong FROM inserted);
-        PRINT N'Lệnh thêm bệnh nhân vào phòng bệnh hợp lệ';
-    END
-END;
+INSERT INTO phong_benh_duoc_su_dung_tai_dich_vu_luu_tru (So_phong, Ma_so_dich_vu_luu_tru) VALUES ('PB21001', 'LT12009');
+INSERT INTO phong_benh_duoc_su_dung_tai_dich_vu_luu_tru (So_phong, Ma_so_dich_vu_luu_tru) VALUES ('PB21001', 'LT12010');
+INSERT INTO phong_benh_duoc_su_dung_tai_dich_vu_luu_tru (So_phong, Ma_so_dich_vu_luu_tru) VALUES ('PB22001', 'LT12011');
+INSERT INTO phong_benh_duoc_su_dung_tai_dich_vu_luu_tru (So_phong, Ma_so_dich_vu_luu_tru) VALUES ('PB22001', 'LT12012');
+INSERT INTO phong_benh_duoc_su_dung_tai_dich_vu_luu_tru (So_phong, Ma_so_dich_vu_luu_tru) VALUES ('PB41001', 'LT14005');
 
 
+SELECT * FROM phong_benh
 
 
-----------------------------------------------------------------------------
------- TỰ ĐỘNG GIẢM hoặc THAY ĐỔI SoLuongBN_HienTai khi delete/update đúng vào table dbo.phong_benh_duoc_su_dung_tai_dich_vu_luu_tru
+--Trường hợp 2: Bệnh nhân vẫn còn hạn sử dụng DV lưu trú, phòng bệnh muốn thêm hết chỗ.
+SELECT * FROM phong_benh
+
+INSERT INTO phong_benh_duoc_su_dung_tai_dich_vu_luu_tru (So_phong, Ma_so_dich_vu_luu_tru) VALUES ('PB21001', 'LT14006');
+
+SELECT * FROM phong_benh
+
+--Trường hợp 3: Bệnh nhân hết hạn sử dụng DV lưu trú, phòng bệnh muốn thêm còn chỗ.
+SELECT * FROM phong_benh
+
+INSERT INTO phong_benh_duoc_su_dung_tai_dich_vu_luu_tru (So_phong, Ma_so_dich_vu_luu_tru) VALUES ('PB21001', 'LT12007');
+
+SELECT * FROM phong_benh
 
 
-CREATE TRIGGER UpdateDeleteSoLuongBenhNhan_phongBenh
-ON dbo.phong_benh_duoc_su_dung_tai_dich_vu_luu_tru
-AFTER UPDATE, DELETE
-AS
-BEGIN
-    SET NOCOUNT ON;
+--Trường hợp 4: Bệnh nhân hết hạn sử dụng DV lưu trú, phòng bệnh muốn thêm hết chỗ.
+SELECT * FROM phong_benh
 
-    -- Trường hợp 1: Giảm số lượng khi xóa ma_so_dich_vu_luu_tru
-    IF EXISTS (SELECT * FROM deleted)
-    BEGIN
-		DECLARE @MaSo_DVLT CHAR(10);
-        SELECT @MaSo_DVLT = Ma_so_dich_vu_luu_tru FROM deleted;
-		/*
-        -- Kiểm tra xem Ma_so_dich_vu_luu_tru có tồn tại trong bảng hay không
-        IF NOT EXISTS (SELECT * FROM phong_benh_duoc_su_dung_tai_dich_vu_luu_tru WHERE Ma_so_dich_vu_luu_tru = @MaSo_DVLT)
-        BEGIN
-            PRINT N'Xoá thất bại hoặc phòng bệnh hiện đang trống!';
-        END
-		*/
-        IF NOT EXISTS (SELECT * FROM inserted) 
-        BEGIN
-            DECLARE @SoPhong_DEL CHAR(10);
-			DECLARE @SoLuongBN_HienTai INT;
-            SELECT @SoPhong_DEL = So_phong FROM deleted;
-			SELECT @SoLuongBN_HienTai = So_luong_benh_nhan_hien_tai FROM dbo.phong_benh WHERE So_phong = @SoPhong_DEL;
-			IF(@SoLuongBN_HienTai > 0)
-				BEGIN
-					 UPDATE dbo.phong_benh
-					 SET So_luong_benh_nhan_hien_tai = So_luong_benh_nhan_hien_tai - 1
-					 WHERE So_phong = @SoPhong_DEL;
-					 PRINT N'Xóa bệnh nhân khỏi phòng bệnh thành công!';
-				END
-			ELSE IF(@SoLuongBN_HienTai <= 0)
-				BEGIN
-					UPDATE dbo.phong_benh
-					SET So_luong_benh_nhan_hien_tai = 0
-					WHERE So_phong = @SoPhong_DEL;
-					RAISERROR(N'Xoá thất bại hoặc phòng bệnh hiện đang trống!', 16,1)
-				END
-        END
-    END
-    
-    -- Trường hợp 2: Cập nhật số lượng khi có sự thay đổi vị trí dịch vụ lưu trú
-    IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted)
-    BEGIN
-        DECLARE @SoPhong_OLD CHAR(10), @SoPhong_NEW CHAR(10);
-        DECLARE @SoBenhNhan INT, @LoaiPhong INT;
-        
-        SELECT @SoPhong_OLD = So_phong FROM deleted;
-        SELECT @SoPhong_NEW = So_phong FROM inserted;
-        SELECT @SoBenhNhan = So_luong_benh_nhan_hien_tai, @LoaiPhong = Loai_phong FROM dbo.phong_benh WHERE So_phong = @SoPhong_NEW;
+INSERT INTO phong_benh_duoc_su_dung_tai_dich_vu_luu_tru (So_phong, Ma_so_dich_vu_luu_tru) VALUES ('PB22001', 'LT12008');
 
-        IF @SoBenhNhan >= @LoaiPhong
-        BEGIN
-            RAISERROR(N'Phòng đã đủ số lượng - Vui lòng chọn phòng khác', 16, 1)
-            ROLLBACK TRANSACTION;
-        END
-        ELSE
-        BEGIN
-            -- Giảm Số lượng BN hiện tại của phòng cũ
-            UPDATE dbo.phong_benh
-            SET So_luong_benh_nhan_hien_tai = CASE 
-                WHEN So_luong_benh_nhan_hien_tai > 0 THEN So_luong_benh_nhan_hien_tai - 1
-                ELSE 0
-            END
-            WHERE So_phong = @SoPhong_OLD;
+SELECT * FROM phong_benh
 
-            -- Tăng số lượng BN hiện tại ở phòng mới 
-            UPDATE dbo.phong_benh
-            SET So_luong_benh_nhan_hien_tai = So_luong_benh_nhan_hien_tai + 1
-            WHERE So_phong = @SoPhong_NEW;
+SELECT * FROM phong_benh_duoc_su_dung_tai_dich_vu_luu_tru
 
-            PRINT N'Thay đổi phòng cho bệnh nhân thành công!';
-        END
-    END
-END;
-GO
+--Trường hợp 5: Xóa bệnh nhân khỏi phòng bệnh nếu bệnh nhân có sử dụng dịch vụ lưu trú.
+SELECT * FROM phong_benh
+
+DELETE phong_benh_duoc_su_dung_tai_dich_vu_luu_tru
+WHERE Ma_so_dich_vu_luu_tru = 'LT14005'
+
+SELECT * FROM ngay_phong_benh_duoc_su_dung_tai_dich_vu_luu_tru
+
+--Trường hợp 6: Xóa bệnh nhân khỏi phòng bệnh nếu bệnh nhân có sử dụng dịch vụ lưu trú.
+SELECT * FROM phong_benh
+
+DELETE ngay_phong_benh_duoc_su_dung_tai_dich_vu_luu_tru
+WHERE Ma_so_dich_vu_luu_tru = 'LT14001'
+
+DELETE phong_benh_duoc_su_dung_tai_dich_vu_luu_tru
+WHERE Ma_so_dich_vu_luu_tru = 'LT14001'
+SELECT * FROM phong_benh
 
 
 
+/*
+INSERT INTO dich_vu_luu_tru (Ma_so, Ngay_bat_dau, Ngay_ket_thuc, Tong_chi_phi)
+VALUES ('LT16011', '2024-04-28', '2024-05-06', 1935000);
 
-------------------------------------------------------------------
------- TỰ ĐỘNG TĂNG/GIẢM/THAY ĐỔI SoLuongBN_HienTai khi có thao tác đến Ngay_ket_thuc của table dbo.dich_vu_luu_tru 
+INSERT INTO phong_benh_duoc_su_dung_tai_dich_vu_luu_tru (So_phong, Ma_so_dich_vu_luu_tru)
+VALUES('PB63001', 'LT16011');
 
-
-CREATE TRIGGER SoLuongBN_HienTai_NgayKetThuc
-ON dbo.dich_vu_luu_tru
-AFTER INSERT, UPDATE, DELETE
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    -- Xử lý trong trường hợp INSERT 
-    IF EXISTS (SELECT * FROM inserted) AND NOT EXISTS (SELECT * FROM deleted)
-    BEGIN 
-        UPDATE pb
-        SET pb.So_luong_benh_nhan_hien_tai = pb.So_luong_benh_nhan_hien_tai + 1
-        FROM phong_benh pb
-        INNER JOIN phong_benh_duoc_su_dung_tai_dich_vu_luu_tru pbd ON pb.So_phong = pbd.So_phong
-        INNER JOIN inserted i ON i.Ma_so = pbd.Ma_so_dich_vu_luu_tru
-        WHERE i.Ngay_ket_thuc > GETDATE();
-    END
-
-    -- Xử lý trong trường hợp DELETE (xóa ko cần điều kiện ngày kết thúc)
-    IF EXISTS (SELECT * FROM deleted) AND NOT EXISTS (SELECT * FROM inserted)
-    BEGIN
-        UPDATE pb
-        SET pb.So_luong_benh_nhan_hien_tai = pb.So_luong_benh_nhan_hien_tai - 1
-        FROM phong_benh pb
-        INNER JOIN phong_benh_duoc_su_dung_tai_dich_vu_luu_tru pbd ON pb.So_phong = pbd.So_phong
-        INNER JOIN deleted d ON d.Ma_so = pbd.Ma_so_dich_vu_luu_tru;
-    END
-
-    -- Xử lý trong trường hợp UPDATE 
-    IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted)
-    BEGIN
-        -- Chỉnh từ [Còn hạn lưu trú] thành hết hạn :V 
-        UPDATE pb
-        SET pb.So_luong_benh_nhan_hien_tai = CASE 
-             WHEN i.Ngay_ket_thuc <= GETDATE() THEN pb.So_luong_benh_nhan_hien_tai - 1
-			 WHEN i.Ngay_ket_thuc <= GETDATE() AND So_luong_benh_nhan_hien_tai < 0 THEN 0
-             ELSE pb.So_luong_benh_nhan_hien_tai
-        END
-        FROM phong_benh pb
-        INNER JOIN phong_benh_duoc_su_dung_tai_dich_vu_luu_tru pbd ON pb.So_phong = pbd.So_phong
-        INNER JOIN inserted i ON i.Ma_so = pbd.Ma_so_dich_vu_luu_tru
-        INNER JOIN deleted d ON d.Ma_so = pbd.Ma_so_dich_vu_luu_tru
-        WHERE d.Ngay_ket_thuc <> i.Ngay_ket_thuc;
+INSERT INTO ngay_phong_benh_duoc_su_dung_tai_dich_vu_luu_tru (So_phong, Ma_so_dich_vu_luu_tru, Ngay_su_dung)
+VALUES ('PB63001', 'LT16011', '2024-04-28');
 
 
-        -- Chỉnh từ [hết hạn] thành  [Còn hạn lưu trú] :V 
-        UPDATE pb
-        SET pb.So_luong_benh_nhan_hien_tai = CASE 
-             WHEN i.Ngay_ket_thuc > GETDATE() THEN pb.So_luong_benh_nhan_hien_tai + 1
-             ELSE pb.So_luong_benh_nhan_hien_tai
-        END
-        FROM phong_benh pb
-        INNER JOIN phong_benh_duoc_su_dung_tai_dich_vu_luu_tru pbd ON pb.So_phong = pbd.So_phong
-        INNER JOIN inserted i ON i.Ma_so = pbd.Ma_so_dich_vu_luu_tru
-        INNER JOIN deleted d ON d.Ma_so = pbd.Ma_so_dich_vu_luu_tru
-        WHERE d.Ngay_ket_thuc <> i.Ngay_ket_thuc;
-		PRINT N'Đã gia hạn/kết thúc DV lưu trú thành công! - Vui lòng kiểm tra lại trong phòng bệnh hiện tại';
-    END
-END;
+----------------
+INSERT INTO phong_benh_duoc_su_dung_tai_dich_vu_luu_tru (So_phong, Ma_so_dich_vu_luu_tru)
+VALUES ('PB41001', 'LT14001')
+
+INSERT INTO ngay_phong_benh_duoc_su_dung_tai_dich_vu_luu_tru (So_phong, Ma_so_dich_vu_luu_tru, Ngay_su_dung)
+VALUES ('PB41001', 'LT14001', '2024-04-01')
+
+*/
+
+SELECT * FROM dich_vu_luu_tru
+SELECT * FROM phong_benh_duoc_su_dung_tai_dich_vu_luu_tru WHERE Ma_so_dich_vu_luu_tru = 'LT14001'
+SELECT * FROM ngay_phong_benh_duoc_su_dung_tai_dich_vu_luu_tru
 
 
+--Trường hợp 7: Chuyển bệnh nhân từ phòng này sang phòng khác (vẫn còn chỗ)
+SELECT * FROM phong_benh
+
+UPDATE dbo.phong_benh_duoc_su_dung_tai_dich_vu_luu_tru
+SET So_phong = 'PB42001'
+WHERE Ma_so_dich_vu_luu_tru = 'LT12012'  
+
+SELECT * FROM phong_benh
+
+--Trường hợp 8: Chuyển bệnh nhân từ phòng này sang phòng khác (đã đầy)
+SELECT * FROM phong_benh
+
+UPDATE dbo.phong_benh_duoc_su_dung_tai_dich_vu_luu_tru
+SET So_phong = 'PB21001'
+WHERE Ma_so_dich_vu_luu_tru = 'LT14005'
+
+SELECT * FROM phong_benh
 
 
+--Trường hợp 9: Insert vào bảng dich_vu_luu_tru
+select * FROM dbo.benh_nhan
+select * FROM dbo.lan_di_benh_vien
+select * FROM dbo.lan_su_dung_dich_vu
+select * FROM dbo.dich_vu_luu_tru
+select * FROM dbo.phong_benh_duoc_su_dung_tai_dich_vu_luu_tru
+select * FROM dbo.phong_benh
+
+INSERT INTO dbo.benh_nhan(Ma_benh_nhan, CCCD,Ho, Ten, Dia_chi, Email, Gioi_tinh, Ngay_sinh)
+VALUES('BN11111', '432653456012', N'Vũ', N'Dũng', N'Đồng Tháp', 'vudung1@gmail.com', 'Nam', '1992-07-25')
+
+
+INSERT INTO dbo.lan_di_benh_vien(Ma_so_lan_di_benh_vien, Gio, Ngay, Tong_chi_phi, Ma_benh_nhan)
+VALUES('LDBV11111', '13:00:00', '2024-04-25', 700000,'BN11111')
+
+INSERT INTO dbo.lan_su_dung_dich_vu(Ma_so, Ma_so_nhan_vien, Ma_so_lan_di_benh_vien, Ma_loai_dich_vu)
+VALUES('LT11111', 'BS10001', 'LDBV11111', 'LT')
+
+INSERT INTO dbo.dich_vu_luu_tru(Ma_so, Ngay_bat_dau, Ngay_ket_thuc, Tong_chi_phi)
+VALUES ('LT11111', '2024-04-25', '2024-04-30',2500000)
+
+UPDATE dbo.dich_vu_luu_tru
+SET Ngay_ket_thuc = '2024-05-30'
+WHERE Ma_so = 'LT11111'
+(QUá ngày - quá hạn DV lưu trú) 
+INSERT INTO dbo.phong_benh_duoc_su_dung_tai_dich_vu_luu_tru(So_phong, Ma_so_dich_vu_luu_tru)
+VALUES('PB41001', 'LT11111')
+
+
+SELECT * FROM dbo.phong_benh;
+
+UPDATE dbo.dich_vu_luu_tru
+SET Ngay_ket_thuc = '2024-04-30'
+WHERE Ma_so = 'LT11111';
+
+SELECT * FROM dbo.phong_benh;
