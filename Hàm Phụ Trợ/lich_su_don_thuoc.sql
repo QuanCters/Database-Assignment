5c6ca187ed2fe5a3840ed82c7bea371c82127330
QuanCters <158025826+QuanCters@users.noreply.github.com>
2024-05-10 19:29:31 +0700

    Update lich_su_don_thuoc.sql

--- Hàm Phụ Trợ/lich_su_don_thuoc.sql
+++ Hàm Phụ Trợ/lich_su_don_thuoc.sql
@@ -5,19 +5,16 @@ returns table
 as 
 return
 (
-	select dvkb.Ma_so, ldbv.Ngay, dt.Tong_tien_thuoc  
+	select dtgt.Ma_don_thuoc, ldbv.Ngay, dt.Tong_tien_thuoc  
 	from
 		don_thuoc as dt,
 		don_thuoc_gom_thuoc as dtgt,
-		dich_vu_kham_benh as dvkb,
 		lan_su_dung_dich_vu as lsddv,
 		lan_di_benh_vien as ldbv
 	where
 		ldbv.Ma_benh_nhan = @id_benh_nhan 
 	AND	ldbv.Ma_so_lan_di_benh_vien = lsddv.Ma_so_lan_di_benh_vien
-	AND	Ma_loai_dich_vu = 'KB'
-	AND	dvkb.Ma_so = lsddv.Ma_so
-	AND dt.Ma_so = lsddv.Ma_so
+	AND 	dt.Ma_so = lsddv.Ma_so
 	AND	dt.Ma_don_thuoc = dtgt.Ma_don_thuoc
 )
 go
